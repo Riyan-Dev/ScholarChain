fa27f7a222b9363ad1fd75a1de3a3392487aaec7
Riyan-Dev <riyan20riyan@icloud.com>
2024-12-11 02:42:16 +0500

    docuemnts backend changed

--- backend/services/langchain_services.py
+++ backend/services/langchain_services.py
@@ -23,7 +23,7 @@ vector_stores = {}
 class LangChainService:
 
     @staticmethod
-    async def create_vector_Store(username: str, newDoc: bool):
+    async def create_vector_Store(username: str, newDoc: bool, keys =  ['CNIC', 'gaurdian_CNIC', 'intermediate_result', 'bank_statements', 'salary_slips', 'gas_bills', 'electricity_bills', 'reference_letter']):
         global vector_stores
 
         if username in vector_stores:
@@ -42,15 +42,18 @@ class LangChainService:
             # Remove from session store
             del vector_stores[username]
 
-       
         user_documents = user["documents"]
-
+        document_list = []
+        for key in keys:
+            if key in user_documents:
+                for doc in user_documents[key]:
+                    document_list.append(doc)
         if not user_documents:
             raise HTTPException(status_code=400, detail=f"No documents found for user {username}.")
 
         # Convert documents into Document objects
         docs_as_documents = [
-            Document(page_content=doc["page_content"], metadata=doc["metadata"]) for doc in user_documents
+            Document(page_content=doc["page_content"], metadata=doc["metadata"]) for doc in document_list
         ]
         
         # Create temporary directory for this session
